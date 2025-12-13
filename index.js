cd5b11063401f3fe589871699d5ddffbbba8bbbf
Hagarth <sancampellone@gmail.com>
2025-12-13 15:45:17 -0300

    Middleware de "No encontrado

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Bienvenidos a mi API!');
+app.use((req, res, next) => {
+    // res.json({ message: 'Hola Middleware!' })
+    console.log(req.method);
+    next();
 })
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Bienvenidos a mi API!' });
+})
+
+import notFound from './src/middlewares/not-found.js';
+
+app.use(notFound)
 
 const port = 3000;
 
